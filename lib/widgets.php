a184065155394102c576ca86981d3d8e7aba5bb9
dmikho <diane@extrasmalldesign.com>
2022-08-30 11:06:08 -0700

    comment out example widget

--- lib/widgets.php
+++ lib/widgets.php
@@ -4,11 +4,11 @@
  */
 function ristretto_widgets_init() {
 	register_sidebar( array(
-		'name' => __( 'News Sidebar', 'ristretto' ),
-		'id' => 'news-sidebar',
-		'description' => __( 'News post sidebar', 'ristretto' ),
-		'before_widget' => '<div id="%1$s" class="news-sidebar %2$s">',
-		'after_widget' => '</div>',
+		// 'name' => __( 'News Sidebar', 'ristretto' ),
+		// 'id' => 'news-sidebar',
+		// 'description' => __( 'News post sidebar', 'ristretto' ),
+		// 'before_widget' => '<div id="%1$s" class="news-sidebar %2$s">',
+		// 'after_widget' => '</div>',
 		// 'before_title' => '<h3 class="ad-title">',
 		// 'after_title' => '</h3>',
 	) );
