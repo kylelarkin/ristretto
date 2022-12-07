4c8c37f7212182f4fdb77c68fde5f1afb30e30b7
dmikho <diane@extrasmalldesign.com>
2022-12-07 10:44:41 -0700

    add aria-label to menu-button

--- components/_menu-button.php
+++ components/_menu-button.php
@@ -1,4 +1,4 @@
 <?php if (!defined('ABSPATH')) exit; ?>
 <div class="menu-button">
-  <a class="menu-toggle" href="#0"><span class="menu-button"></span></a>
+  <a class="menu-toggle" href="#0" aria-label="See the Navigation"><span class="menu-button"></span></a>
 </div>
