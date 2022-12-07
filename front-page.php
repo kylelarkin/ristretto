02eeec2597ee8e48a9cdcfaf22e0651d71b18f26
dmikho <diane@extrasmalldesign.com>
2022-12-07 10:50:44 -0700

    remove title from homepage, clean up function registration order

--- front-page.php
+++ front-page.php
@@ -7,12 +7,10 @@
 	<main class="standard-grid">
 
 	<?php if(have_posts()) : while(have_posts()) : the_post(); // start loop ?>
+		
+		<?php the_content(); ?>
 	
-  	<h1><?php the_title(); ?></h1>
-	
-  	<?php the_content(); ?>
-	
-  <?php endwhile; endif; // end loop ?>
+	<?php endwhile; endif; // end loop ?>
 
 	</main>
 
--- functions.php
+++ functions.php
@@ -11,35 +11,37 @@ foreach (glob(dirname(__FILE__) . '/lib/' . '*.php') as $filename) {
  */
 function ristretto_enqueue_init() {
   // Register Scripts
-  wp_register_script( 'fontawesome', '//kit.fontawesome.com/xxxxxxxxxx.js', null, null, true );
   wp_register_script( 'aos', get_bloginfo( 'stylesheet_directory' ) . '/node_modules/aos/dist/aos.js', null, null, true );
-  wp_register_script( 'object-fit-images', get_bloginfo( 'stylesheet_directory' ) . '/js/dist/ofi.min.js', null, null, true );
+  wp_register_script( 'fontawesome', '//kit.fontawesome.com/xxxxxxxxxx.js', null, null, true );
   wp_register_script( 'jarallax', get_bloginfo( 'stylesheet_directory' ) . '/node_modules/jarallax/dist/jarallax.min.js', array('jquery'), null, true );
-  wp_register_script( 'tiny-slider', get_bloginfo( 'stylesheet_directory' ) . '/node_modules/tiny-slider/dist/min/tiny-slider.js', null, null, true );
   wp_register_script( 'lity', get_bloginfo( 'stylesheet_directory' ) . '/node_modules/lity/dist/lity.min.js', array('jquery'), null, true );
+  wp_register_script( 'object-fit-images', get_bloginfo( 'stylesheet_directory' ) . '/js/dist/ofi.min.js', null, null, true );
   wp_register_script( 'object-fit', get_bloginfo( 'stylesheet_directory' ) . '/js/dist/ofi.min.js', array('jquery'), null, true );
+  wp_register_script( 'tiny-slider', get_bloginfo( 'stylesheet_directory' ) . '/node_modules/tiny-slider/dist/min/tiny-slider.js', null, null, true );
   wp_register_script( 'ristretto-app', get_bloginfo( 'stylesheet_directory' ) . '/js/dist/app.js', array('jquery'), null, true );
+  
   // Enqueue Scripts
-  wp_enqueue_script('fontawesome');
   wp_enqueue_script('aos');
-  wp_enqueue_script('object-fit-images');
+  wp_enqueue_script('fontawesome');
   wp_enqueue_script('jarallax');
-  wp_enqueue_script('tiny-slider');
   wp_enqueue_script('lity');
+  wp_enqueue_script('object-fit-images');
   wp_enqueue_script('object-fit');
+  wp_enqueue_script('tiny-slider');
   wp_enqueue_script('ristretto-app');
 
   // Register Styles
-  wp_register_style( 'typekit', '//use.typekit.net/cgn2aav.css', array(), null, 'all' );
+  wp_register_style( 'aos-styles', get_bloginfo( 'stylesheet_directory' ) . '/node_modules/aos/dist/aos.css', array(), null, 'all' );
   wp_register_style( 'lity-css', get_bloginfo( 'stylesheet_directory' ) . '/node_modules/lity/dist/lity.min.css', array(), null, 'all' );
   wp_register_style( 'tiny-slider-css', get_bloginfo( 'stylesheet_directory' ) . '/node_modules/tiny-slider/dist/tiny-slider.css', array(), null, 'all' );
-  wp_register_style( 'aos-styles', get_bloginfo( 'stylesheet_directory' ) . '/node_modules/aos/dist/aos.css', array(), null, 'all' );
+  wp_register_style( 'typekit', '//use.typekit.net/cgn2aav.css', array(), null, 'all' );
   wp_register_style( 'ristretto-screen', get_bloginfo( 'stylesheet_directory' ) . '/css/screen.css', array(), '1.0.1', 'all' );
+  
   // Enqueue Styles
-  wp_enqueue_style('typekit');
+  wp_enqueue_style('aos-styles');
   wp_enqueue_style('lity-css');
   wp_enqueue_style('tiny-slider-css');
-  wp_enqueue_style('aos-styles');
+  wp_enqueue_style('typekit');
   wp_enqueue_style('ristretto-screen');
  }
 add_action('wp_enqueue_scripts', 'ristretto_enqueue_init', 15);
