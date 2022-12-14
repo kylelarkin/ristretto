e9b376d05e758878633cdc9f2f763811e8dccba4
dmikho <diane@extrasmalldesign.com>
2022-12-14 08:32:12 -0700

    add styles for visually hidden, add color palette mixin to default

--- lib/admin.php
+++ lib/admin.php
@@ -7,24 +7,20 @@ define( 'DISALLOW_FILE_EDIT', true );
 /**
  * Add custom logo to Wordpress Login page(s). Logo should be no bigger than 323 pixels wide by 67 pixels high
  */
-function ristretto_login_logo() { ?>
-    <style type="text/css">
-        body.login div#login h1 a {
-            background: url('<?php bloginfo( 'template_directory' ) ?>/img/wp-login-logo-ristretto.png') no-repeat 0 0;
-            background-size: contain;
-            margin: 0 0 0 23px;
-            width: 274px;
-        }
-    </style>
-<?php }
+function ristretto_login_logo() {
+
+  wp_register_style( 'login-style', get_bloginfo( 'stylesheet_directory' ) . '/css/_login.css', array(), null, 'all' );
+  wp_enqueue_style('login-style');
+  
+}
 add_action( 'login_enqueue_scripts', 'ristretto_login_logo' );
 
 /**
  * Allow SVG files to be uploaded to media library
  */
 function cc_mime_types( $mimes ){
-	$mimes['svg'] = 'image/svg+xml';
-	return $mimes;
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
 }
 add_filter( 'upload_mimes', 'cc_mime_types' );
 
@@ -37,17 +33,6 @@ function ristretto_hide_admin_bar() {
   }
 }
 add_action('set_current_user', 'ristretto_hide_admin_bar');
-
-/**
- * Prevent Subscribers from seeing dashboard
- */
-function ristretto_subscriber_redirect(){
-  if( is_admin() && !defined('DOING_AJAX') && current_user_can('subscriber') ){
-    wp_redirect( home_url() . '/myaccount' );
-    exit;
-  }
-}
-add_action('init','ristretto_subscriber_redirect');
 
 /**
  * Change Login Logo to point to Home Page
