3675e7166a23209f4c0836eda8eddefb9808d586
Kyle Larkin <kyle@extrasmalldesign.com>
2024-12-12 15:48:35 -0700

    Sass Dart Restructuring and Theme JSON Updates

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 	<?php if (!defined('ABSPATH')) exit;	?>
   
-	<footer class="body--footer is-layout-constrained">
+	<footer class="body--footer is-layout-constrained has-global-padding">
 		<nav class="body--footer--nav">
 			<?php wp_nav_menu( array( 'theme_location' => 'primary-footer-menu-location', 'container' => false) ); ?>
 		</nav>
