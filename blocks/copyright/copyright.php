5aae2f6173a02a43f82a217a5e7b6d90faddaa67
dmikho <diane@extrasmalldesign.com>
2025-04-30 09:31:50 -0700

    add copyright block, remove unncessary css folder

---
+++ blocks/copyright/copyright.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Social Sharing Block Template.
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during AJAX preview.
+ * @param   (int|string) $post_id The post ID this block is saved to.
+ */
+
+// Create id attribute allowing for custom "anchor" value.
+$id = 'block-copyright-' . $block['id'];
+if( !empty($block['anchor']) ) {
+		$id = $block['anchor'];
+}
+
+// Create class attribute allowing for custom "className", "align", "background" values.
+$className = 'block-copyright';
+
+if( !empty($block['className']) ) {
+		$className .= ' ' . $block['className'];
+}
+?>
+
+<div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
+  <p class="copyright">&copy;<?php echo date("Y"); ?> <em>The Carryall</em> & contributors</p>
+</div>
