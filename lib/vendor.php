445880ddf2631e6dbea8f43cef495d6c66f885b1
dmikho <diane@extrasmalldesign.com>
2025-08-11 16:09:27 -0700

    Disable SearchWP Email Summaries

--- lib/vendor.php
+++ lib/vendor.php
@@ -2,16 +2,16 @@
 /**
  * Add ACF Google Maps API Key
  */
-function my_acf_init() { 
+function my_acf_init() {
   acf_update_setting('google_api_key', '');
 }
 //add_action('acf/init', 'my_acf_init');
 
-/** 
- * Removes Events from WP Admin Bar  
+/**
+ * Removes Events from WP Admin Bar
  */
 define('TRIBE_DISABLE_TOOLBAR_ITEMS', true);
- 
+
  // Add Data Attribute for FontAwesome Pseudo Elements
  function add_data_attribute($tag, $handle) {
    if ( 'fontawesome' !== $handle )
@@ -19,7 +19,7 @@ define('TRIBE_DISABLE_TOOLBAR_ITEMS', true);
    return str_replace( ' src', ' data-search-pseudo-elements defer src', $tag );
  }
  add_filter('script_loader_tag', 'add_data_attribute', 10, 2);
- 
+
  /**
    * Move Yoast Panel to Bottom of Editor
   */
@@ -27,8 +27,11 @@ define('TRIBE_DISABLE_TOOLBAR_ITEMS', true);
      return 'low';
  }
  add_filter( 'wpseo_metabox_prio', 'move_yoast_to_bottom');
- 
-/** 
- * remove Imagify from toolbar 
+
+/**
+ * remove Imagify from toolbar
  */
 add_filter( 'pre_get_imagify_option_admin_bar_menu', '__return_false' );
+
+// Disable SearchWP Email Summaries
+ add_filter( 'searchwp/email_summaries/disabled', '__return_true');
