dcaee02ad4c0d6105a9d2f3d69920a7c252bbe8b
Diane Padilla <diane@extrasmalldesign.com>
2026-06-01 15:41:14 -0700

    update fontawesome in editor to prevent lag

--- lib/editor.php
+++ lib/editor.php
@@ -16,13 +16,14 @@ function ristretto_enqueue_gutenberg() {
   // Enqueue Typekit for Editor.
   wp_register_style( 'ristretto-gutenberg-fonts', '//use.typekit.net/vbl0nii.css' );
   // wp_register_style( 'tiny-slider-css', get_bloginfo( 'stylesheet_directory' ) . '/node_modules/tiny-slider/dist/tiny-slider.css', array(), null, 'all' );
+  
   wp_enqueue_style( 'ristretto-gutenberg-fonts' );
   // wp_enqueue_style( 'tiny-slider-css' );
-  // Font Awesome for Editor
-  wp_register_script( 'fontawesome', '//kit.fontawesome.com/ce9172c803.js', null, null, true );
+
   wp_register_script( 'editor-tiny-slider', get_bloginfo( 'stylesheet_directory' ) . '/node_modules/tiny-slider/dist/min/tiny-slider.js', null, null, true );
+  
   // wp_enqueue_script('editor-tiny-slider');
-  wp_enqueue_script('fontawesome');
+
 }
 add_action( 'enqueue_block_editor_assets', 'ristretto_enqueue_gutenberg' );
 
@@ -31,3 +32,26 @@ add_theme_support( 'responsive-embeds' );
 
 /* Remove default block patterns */
 remove_theme_support( 'core-block-patterns' );
+
+/**
+ * Add FontAwesome resource hints in the admin.
+ */
+function ristretto_resource_hints( $urls, $relation_type ) {
+  if ( ! is_admin() ) {
+    return $urls;
+  }
+
+  if ( 'preconnect' === $relation_type ) {
+    $urls[] = array(
+      'href'        => 'https://kit.fontawesome.com',
+      'crossorigin' => 'anonymous',
+    );
+  }
+
+  if ( 'dns-prefetch' === $relation_type ) {
+    $urls[] = 'https://ka-f.fontawesome.com';
+  }
+
+  return $urls;
+}
+add_filter( 'wp_resource_hints', 'ristretto_resource_hints', 10, 2 );
